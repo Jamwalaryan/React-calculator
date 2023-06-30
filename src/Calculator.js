c7b45523e8e976b650dea342067c40f9204d7515
Jamwalaryan <aryanjamwal29@gmail.com>
2023-06-30 16:27:28 +0530

    calculator

---
+++ src/Calculator.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function Calculator() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (event) => {
+    setNum1(event.target.value);
+  };
+
+  const handleNum2Change = (event) => {
+    setNum2(event.target.value);
+  };
+
+  const handleAddition = () => {
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(sum);
+  };
+
+  const handleSubtraction = () => {
+    const difference = parseFloat(num1) - parseFloat(num2);
+    setResult(difference);
+  };
+
+  const handleMultiplication = () => {
+    const product = parseFloat(num1) * parseFloat(num2);
+    setResult(product);
+  };
+
+  const handleDivision = () => {
+    const quotient = parseFloat(num1) / parseFloat(num2);
+    setResult(quotient);
+  };
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <input type="number" value={num1} onChange={handleNum1Change} />
+      <input type="number" value={num2} onChange={handleNum2Change} />
+      <br />
+      <button onClick={handleAddition}>Addition</button>
+      <button onClick={handleSubtraction}>Subtraction</button>
+      <button onClick={handleMultiplication}>Multiplication</button>
+      <button onClick={handleDivision}>Division</button>
+      <br />
+      <h2>Result: {result}</h2>
+    </div>
+  );
+}
+
+export default Calculator;
